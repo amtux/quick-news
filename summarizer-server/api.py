cb8303be70d63615d94c06c1e699bf9bf3d74e59
amtux <aman.singh.uppal@gmail.com>
2015-06-26 19:24:55 -0400

    backend functional

--- summarizer-server/api.py
+++ summarizer-server/api.py
@@ -1,4 +1,24 @@
-from eve import Eve
-app = Eve()
- 
-app.run();
+# codinsummaries = SummarizeUrl(url)g: utf-8
+from flask import Flask, jsonify, request
+from pyteaser import SummarizeUrl
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Flask is running!'
+
+
+@app.route('/data')
+def names():
+	if 'url' in request.args:
+		url = request.args['url']
+		summaries = SummarizeUrl(url)
+		return json.dumps(summaries)
+	else:
+		return "false"
+
+if __name__ == '__main__':
+	app.run(debug=True)

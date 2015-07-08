45ae838425d727d13c85dad42c3d2a398bc1fddc
amtux <aman.singh.uppal@gmail.com>
2015-07-07 23:41:54 -0400

    show bbc news logo if img requested not found

---
+++ app/scripts/directives/errsrc.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name quickNewsApp.directive:errSrc
+ * @description
+ * # errSrc
+ */
+angular.module('quickNewsApp')
+  .directive('errSrc', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+      	element.bind('error', function() {
+            if (attrs.src != attrs.errSrc) {
+              attrs.$set('src', attrs.errSrc);
+            }
+        });
+      }
+    };
+  });
